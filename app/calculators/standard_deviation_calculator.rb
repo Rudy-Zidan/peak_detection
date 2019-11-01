c9bba6db96313748082232e6854c9854a7b836fe
Rudy Zidan <rz.zidan@gmail.com>
2019-11-01 22:23:59 +0200

    Implement standard deviation calculator.

---
+++ app/calculators/standard_deviation_calculator.rb
@@ -0,0 +1,16 @@
+class StandardDeviationCalculator < ApplicationCalculator
+  def initialize(list:, mean:)
+    @list = list
+    @mean = mean
+  end
+
+  def calculate
+    return 0.0 if @list.nil? || @list.empty? || @mean.nil?
+
+    result = @list.reduce(0.0) do |acc, value|
+      acc.to_f + ((value.to_f - @mean) ** 2)
+    end
+
+    Math.sqrt(result / @list.size.to_f)
+  end
+end
---
+++ test/calculators/standard_deviation_calculator_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+
+class StandardDeviationCalculatorTest < ActiveSupport::TestCase
+  test "should load the calculator" do
+    assert StandardDeviationCalculator.new(list: [], mean: nil)
+  end
+
+  test "should return 0.0 if list empty" do
+    stddv = StandardDeviationCalculator.calculate(list: [], mean: nil)
+
+    assert_equal(stddv, 0.0)
+  end
+
+  test "should return 0.0 if list nil" do
+    stddv = StandardDeviationCalculator.calculate(list: nil, mean: nil)
+
+    assert_equal(stddv, 0.0)
+  end
+
+  test "should calculate mean of all readings" do
+    data = Reading.pluck(:value)
+    mean = MeanCalculator.calculate(list: data)
+    stddv = StandardDeviationCalculator.calculate(list: data, mean: mean)
+
+    assert_equal(stddv, 4.5)
+  end
+end
